e497c50ba233794de9ff1aa20c22b3082d7a9b87
DiegoVeraSarricolea <d.vera.sarricolea@gmail.com>
2022-01-18 11:27:03 -0300

    NavBar Menu

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,8 +4,8 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 
 const products = [
-    {id: 1, name: "Delantal Ortopédico", description: "Descripción del delantal", price: '$12000'},
-    {id: 2, name: "Bota Ortopédica", description: "Descripción de la Bota", price: '$18000'},
+    {id: 1, name: "Delantal Ortopédico", description: "Descripción del delantal", price: '$12000', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1cNmzUP_W-9Woh9TwKPs2hXs_IdUzNbrIbV_Ksnx_bJPfGLbawghE-Tb7ZpD8x6xTQ7g&usqp=CAU"},
+    {id: 2, name: "Bota Ortopédica", description: "Descripción de la Bota", price: '$18000', image:"https://dipromed.cl/d/ortopedia/1411-thickbox_default/bota-ortopedica-baja.jpg"},
 ]
 
 const Products = () => {
